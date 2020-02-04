d68a6b891c74dc6a7505f4e85f0d586a3185f2a3
lferracini <laercio_ferracini@yahoo.com.br>
2020-02-03 22:42:16 -0300

    O Primeiro Script

---
+++ meuscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Um comentário
+
+echo "Início" 
+: <<'COMENTARIODOC'
+
+Comentario de várias linhas
+
+COMENTARIODOC
+echo "Fim"
+echo "Hello shell world" #Comment line
+
+echo -e \\
